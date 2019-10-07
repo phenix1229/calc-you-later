2d3b6459156882653b3d4b94c8d0fe15dcd517cf
phenix1229 <paul.garay@codeimmersives.com>
2019-10-07 13:51:53 -0400

    completed tests

--- calculate.js
+++ calculate.js
@@ -1,5 +1,22 @@
-function calculate() {
-  
+function calculate(num1, num2, operation) {
+    const n1 = Number(num1);
+    const n2 = Number(num2);
+
+    if (operation === '+' || operation === 'plus'){
+        result = n1 + n2;
+    } else if (operation === '-' || operation === 'minus'){
+        result = n1 - n2;
+    } else if (operation === 'x' || operation === 'times' || operation === 'X'){
+        result = n1 * n2;
+    } else if (operation === '/'){
+        result = n1 / n2;
+    } else if (operation === '%' || operation === 'mod' || operation === 'modulus'){
+        result = n1 % n2;
+    } else {
+        result = 'Sorry, that\'s not a mathematical operation!'
+    }
+
+  return result
 }
 
 
