40ab9f37c207e0e007ca8900bbde8a7986b17433
phenix1229 <paul.garay@codeimmersives.com>
2019-10-08 12:19:57 -0400

    added and completed new tests

--- calculate.js
+++ calculate.js
@@ -2,13 +2,13 @@ function calculate(num1, num2, operation) {
     const n1 = Number(num1);
     const n2 = Number(num2);
 
-    if (operation === '+' || operation === 'plus'){
+    if (operation === '+' || operation === 'plus' || operation === 'added to'){
         result = n1 + n2;
-    } else if (operation === '-' || operation === 'minus'){
+    } else if (operation === '-' || operation === 'minus' || operation === 'subtracted from'){
         result = n1 - n2;
-    } else if (operation === 'x' || operation === 'times' || operation === 'X'){
+    } else if (operation === 'x' || operation === 'times' || operation === 'X' || operation === 'multiplied by'){
         result = n1 * n2;
-    } else if (operation === '/'){
+    } else if (operation === '/' || operation === 'divided by'){
         result = n1 / n2;
     } else if (operation === '%' || operation === 'mod' || operation === 'modulus'){
         result = n1 % n2;
